3901c5dfce1671985a79e2ac3b120bd45244b860
mamudsamiul <samiulmamud1995@gmail.com>
2020-09-24 10:44:33 +0530

    (Samiul Mamud) added : Welcome Message

---
+++ src/com/cg/day3/EmployeeWageProblem.java
@@ -0,0 +1,10 @@
+package com.cg.day3;
+
+public class EmployeeWageProblem {
+
+	public static void main(String[] args) {
+		System.out.println(": Welcome to Employee Wage calculation Program : ");
+
+	}
+
+}
