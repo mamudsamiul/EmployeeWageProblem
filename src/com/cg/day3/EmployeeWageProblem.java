4a5c7c76ad8fda21e512f3337c7d592cc2712bda
mamudsamiul <samiulmamud1995@gmail.com>
2020-09-24 11:25:17 +0530

    (Samiul Mamud) added : Daily Wage update is added

--- src/com/cg/day3/EmployeeWageProblem.java
+++ src/com/cg/day3/EmployeeWageProblem.java
@@ -8,6 +8,10 @@ public class EmployeeWageProblem {
 		
 		//Constant
 		int IS_FULL_TIME=1;
+		int EMP_RATE_PER_HOUR=200;
+		
+		//Variable
+		int empHrs=0,empWage=0;
 		
 		//Welcome Message
 		
@@ -21,13 +25,14 @@ public class EmployeeWageProblem {
 		double empCheck=rand_number%2;
 		if(IS_FULL_TIME==empCheck)
 		{
-			System.out.println("Employee is present ");
+			empHrs=8;
 		}
 		else
 		{
-			System.out.println("Employee is absent ");
+			empHrs=0;
 		}
-
+		empWage=empHrs*EMP_RATE_PER_HOUR;
+		System.out.println("Daily wage is : "+empWage);
 	}
 
 }
