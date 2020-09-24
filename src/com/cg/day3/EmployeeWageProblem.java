58d722038eb2e610790f0a4bd14069adcd5e06ea
mamudsamiul <samiulmamud1995@gmail.com>
2020-09-24 11:46:44 +0530

    (Samiul Mamud) Modified: Logic established by switch case

--- src/com/cg/day3/EmployeeWageProblem.java
+++ src/com/cg/day3/EmployeeWageProblem.java
@@ -7,9 +7,9 @@ public class EmployeeWageProblem {
 	public static void main(String[] args) {
 		
 		//Constant
-		int IS_FULL_TIME=1;
-		int IS_PART_TIME=2;
-		int EMP_RATE_PER_HOUR=200;
+		final int IS_FULL_TIME=1;
+		final int IS_PART_TIME=2;
+		final int EMP_RATE_PER_HOUR=200;
 		
 		//Variable
 		int empHrs=0,empWage=0;
@@ -23,19 +23,19 @@ public class EmployeeWageProblem {
 		int rand_number=rand.nextInt(100);//Generating random number in between 0 and 100
 		
 		
-		double empCheck=rand_number%3;
-		if(empCheck==IS_FULL_TIME)
-		{
-			empHrs=8;
-		}
-		else if(empCheck==IS_PART_TIME)
-		{
-			empHrs=4;
-		}
-		else
-		{
-			empHrs=0;
-		}
+		int empCheck=rand_number%3;
+		switch(empCheck) {
+			case IS_FULL_TIME:
+				empHrs=8;
+				break;
+			case IS_PART_TIME:
+				empHrs=4;
+				break;
+			default:
+				empHrs=0;
+			
+				
+		}		
 		empWage=empHrs*EMP_RATE_PER_HOUR;
 		System.out.println("Daily wage is : "+empWage);
 	}
